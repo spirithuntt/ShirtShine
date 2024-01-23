d61f2004cecc77790714d5bdcf3de8a89102eeac
Nouhaila El Housni <112879456+spirithuntt@users.noreply.github.com>
2024-01-23 14:45:54 +0100

    init

---
+++ src/main/java/youcode/shirtshine/ShirtShineApplication.java
@@ -0,0 +1,13 @@
+package youcode.shirtshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirtShineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirtShineApplication.class, args);
+    }
+
+}
